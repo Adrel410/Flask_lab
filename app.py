dc65fec0fa2cbf381effd03eb4b1da39323cb97a
Adrel410 <ogundipe_adeola@columbusstate.edu>
2023-09-08 19:12:00 +0000

    Completed the basic concept

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask           # import flask framework
+from flask import Flask, render_template   
+from flask import Flask, render_template, request
+
 app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # use the home url
 def hello():                      # method called hello
-    return "Hello World!"         # returns "hello world"
+   return render_template("index.html")         # returns "hello world"
+    
+@app.route("/<name>")              # route with URL variable /<name>
+def hello_name(name):              # call method hello_name
+    name = request.args.get('Adeola')
+    return "Hello "+ name          # which returns "hello + name  
+
+@app.route("/about")
+def about():
+    name = request.args.get('Adeola') if request.args.get('name') else "Hello World!" 
+    return render_template("about.html", aboutName=name)   
+    
 if __name__ == "__main__":        # when running python app.py
     #app.run()                     # run the flask app
     #change app.run() to the following
